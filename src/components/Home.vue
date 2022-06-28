b24c2985731f05e8bf5e014ff88be1bf13244d4c
chriiss <christopher.kumla@gmail.com>
2022-06-28 09:52:48 +0200

    age const replace let

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -28,7 +28,7 @@ export default {
     getAge(ageDate){
       const today = new Date();
       const birthDate = new Date(ageDate);
-      const age = today.getFullYear() - birthDate.getFullYear();
+      let  age = today.getFullYear() - birthDate.getFullYear();
       const m = today.getMonth() - birthDate.getMonth();
       if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate()))age = age--
       return age;
